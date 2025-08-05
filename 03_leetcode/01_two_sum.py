7ba4557527f23664b4a4672f7bc4bf50d9fb7f7a
Ivancito <e_2022100487f@uncp.edu.pe>
2025-08-05 12:22:07 -0500

    primer ejercicio d eleetcode

---
+++ 03_leetcode/01_two_sum.py
@@ -0,0 +1,16 @@
+
+nums = [2,7,11,15]
+
+target = 9
+
+def two_sum(nums, target):
+    
+    for i in range(len(nums)):
+        for j in range(i + 1, len(nums)):
+            sum = nums[i] + nums[j]
+            
+            if sum == target:
+                return [i, j]
+                
+print(two_sum(nums,target))
+
